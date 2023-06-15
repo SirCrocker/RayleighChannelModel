371242411e17023398fd2b66135e7b84991b1363
Diego Torreblanca <diego.torreblanca77@gmail.com>
2023-06-15 14:22:09 -0400

    Update: channel_estimate.m y create_channel.m
    Se actualizó la función channel_estimate.m para que devuelva en un arreglo las 4 interpolaciones, realizando el cálculo de los vectores una sola vez y de forma ordenada. Por otro lado, acreate_channel.m se le hicieron cambios estéticos.

--- channel_estimate.m
+++ channel_estimate.m
@@ -1,30 +1,19 @@
 %% Estimación del canal
-% Genera los coeficientes estimados el canal según la interpolación elegida
-% para realizar la ecualización
+% Genera los coeficientes estimados del canal según las interpolaciones
+% lineal, fft, spline y pchip para realizar la ecualización
 % rxp: vector de símbolos piloto
 % pilot: símbolo piloto definido
-% interpolation: interpolación elegida
-% nb: cantidad de símbolos
+% nb: cantidad de símbolos de mensaje
 % N: separación de símbolos piloto
 
-function cse = channel_estimate(rxp,pilot,interpolation, nb, N)
-
-    csi=rxp/pilot; % Calculo del csi de la señal piloto
-    if strcmp(interpolation,'fft')
-        cse=interpft(csi,nb);on
-    else
-        total_symbols = nb * N / (N-1);
-        t1=1:1:total_symbols; % Todos los símbolos
-        t2=1:N:total_symbols;% Posiciones de los piloto
-        m=1;
-        for i=1:N:total_symbols
-         t3(m:m+(N-2))=t1( i+1:i+(N-1));% Solo posiciones de símbolos de mensaje
-         m=m+(N-1);
-        end
-        try 
-            cse=interp1(t2,csi,t3,interpolation);
-        catch
-            error('Ingrese una interpolación válida: fft, spline, linear o pchip.')
-        end
-    end
+function [fft, spline, linear, pchip] = channel_estimate(rxp,pilot,nb, N)
+    csi = rxp/pilot; % Calculo del csi de la señal piloto
+    total_symbols = ceil(nb * N / (N-1));
+    t1 = 1:1:total_symbols; % Todos los símbolos
+    t2 = 1:N:total_symbols; % Posiciones de los símbolos piloto
+    t3 = t1(mod(t1, N) ~= 1); % Posiciones de los símbolos de mensaje
+    fft = interpft(csi,nb); % interpolación fft
+    spline = interp1(t2,csi,t3,'spline'); % interpolación spline (cubic)
+    linear = interp1(t2,csi,t3,'linear'); % interpolación lineal
+    pchip = interp1(t2,csi,t3,'pchip'); % interpolación pchip (cubic)
 end
--- create_channel.m
+++ create_channel.m
@@ -28,19 +28,19 @@ function channel_coeffs=create_channel(varargin)
         lambda = 3e8/f_c; %longitud de onda de la portadora
         v = speed/3.6; %velocidad del móvil UE m/s
         fmax = v/lambda; % Max doppler shift
-        A=1; % Amplitude
-        f=1e4; % Frecuencia de muestreo
+        A = 1; % Amplitude
+        f = 1e4; % Frecuencia de muestreo
         % Tiempo ajustado para que los gráficos se vean bien
         last_time = 10^(log10(f)-4)*((n_coeffs/f)-(1/f));
         t = linspace(0,last_time,n_coeffs);
-        ct=zeros(1,n_coeffs);
-        ph=2*pi* rand(1,paths);
-        theta=2*pi*rand(1,paths);
-        fd=fmax*cos(theta); %doppler shift
+        ct = zeros(1,n_coeffs);
+        ph = 2*pi* rand(1,paths);
+        theta = 2*pi*rand(1,paths);
+        fd = fmax*cos(theta); %doppler shift
         for k=1:paths
-        ct=ct+A*exp(1i*(2*pi*fd(k)*t+ph(k)));
+            ct = ct+A*exp(1i*(2*pi*fd(k)*t+ph(k)));
         end
-        channel_coeffs=ct/sqrt(paths); % Coeficientes del canal
+        channel_coeffs = ct/sqrt(paths); % Coeficientes del canal
     else
         error(['Se recibieron ', num2str(nargin), ' argumentos. Solamente se puede ingresar 1 argumento para ' ...
             'Canal multipath Rayleigh multiplicativo (infinitos paths) o 4 argumentos para Rayleigh Fading.']);
