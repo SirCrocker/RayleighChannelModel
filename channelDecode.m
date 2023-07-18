1dda313b5b4abe300404714d8ba791a9e02e8afa
Agustin G. <agustin.gonzalez.u@gmail.com>
2023-07-17 23:29:52 -0400

    Add: Channel encoding files

---
+++ channelDecode.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = channelDecode(inputArg1,inputArg2)
+%CHANNELDECODE Decodifica la codificación de canal
+%   Implementado en función de una codificación de canal Hamming 7,4
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ channelEncode.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = channelEncode(inputArg1,inputArg2)
+%CHANNELENCODE Aplica codificación de canal a los bits a enviar
+%   Se utiliza Hamming 7,4 para codificar los bits
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
