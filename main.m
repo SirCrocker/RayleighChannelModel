685d14dd8773bff7f660a40ab17722cfcdb6f956
Agustin G. <agustin.gonzalez.u@gmail.com>
2023-07-23 22:05:55 -0400

    Fix: added axis labels.

--- main.m
+++ main.m
@@ -73,6 +73,8 @@ for modulation = modulations
         xlim([-2, 30]);
         grid on
         legend(["Linear", "Perfect", "Linear Enc.", "Perfect Enc."])
+        xlabel("$$\frac{E_b}{N_0}$$", "Interpreter","latex")
+        ylabel("BER")
     end
     sgtitle("BER Curves for " + modulation + " and scenario " + num2str(n_scene), 'FontSize', 20)
     exportgraphics(fig, fullfile(folder_plots, modulation + "_BOTHPILOTS" + "_SCENE_" + num2str(n_scene) + "_WITHENCDN_" + ".png"), 'Resolution', 300)
